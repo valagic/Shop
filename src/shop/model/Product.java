00bdcc82884557a4b1976f1db3e3499e1e276ec7
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-10-06 12:12:55 +0200

    Made model product

---
+++ src/shop/model/Product.java
@@ -0,0 +1,37 @@
+package shop.model;
+
+public class Product {
+
+	private int sku;
+	private String name;
+	private int quantity;
+	private Double price;
+	
+	public int getSku() {
+		return sku;
+	}
+	public void setSku(int sku) {
+		this.sku = sku;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+
+	
+}
